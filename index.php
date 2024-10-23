8fdc4a7a6cb722a526d7e2ad6c0e0165b5233f4c
Traxus Laptop <l21530323@cancun.tecnm.mx>
2024-10-23 11:13:56 -0600

    Change css relative path

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include '.gitignore/config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HammaCraft</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/stylesHenrry.css">
 
 </head>
 
@@ -102,7 +102,7 @@ include '.gitignore/config.php';
             <div class="carousel-inner">
 
                 <?php
-                $carouselProductCount = 12;
+                $carouselProductCount = 15;
                 $sql = "SELECT * FROM productos LIMIT $featuredProductsCount, $carouselProductCount";
                 $result = $connection->query($sql);
 
