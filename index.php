d0797761cc6e77d1a0edd0955180fad5631136b9
Trashah <l21530323@cancun.tecnm.mx>
2024-10-23 19:41:22 -0500

    Rename stylesHenrry.css to styles.css

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include 'php/functions.php'
     <meta charset="UTF-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
     <title> HammaCraft </title>
-    <link rel="stylesheet" href="css/stylesHenrry.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
 </head>
