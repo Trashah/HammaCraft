58a0a9494db73629fe45539627e4ffff8b9ca53e
Andy-LH123 <116239005+Andy-LH123@users.noreply.github.com>
2024-10-24 17:50:33 -0500

    update catalogo

--- php/catalogo.php
+++ php/catalogo.php
@@ -11,6 +11,7 @@ include '../php/functions.php';
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+    <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <style>
         body {
