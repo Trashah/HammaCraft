9d4161fdf639a9ca30539e860ccb0f9108d1c0a3
Trashah <l21530323@cancun.tecnm.mx>
2024-10-29 00:30:54 -0500

    update pedidos.php

--- php/pedidos.php
+++ php/pedidos.php
@@ -64,34 +64,21 @@ include '../php/functions.php';
     </main>
 
     <footer>
-
         <div class="footer-content">
-
             <div class="footer-section">
-
                 <h3>MENU</h3>
-
                 <ul>
-
                     <li><a href="https://www.hammacraft.lat">Pantalla principal</a></li>
                     <li><a href="https://www.hammacraft.lat/php/catalogo.php">Tienda</a></li>
-
             <div class="footer-section">
-
                 <h3>Siguenos</h3>
-
                 <ul>
-
                     <li><a href="#facebook">Facebook</a></li>
                     <li><a href="#twitter">Twitter</a></li>
                     <li><a href="#instagram">Instagram</a></li>
-
                 </ul>
-
             </div>
-
         </div>
-
     </footer>
 
 </body>
