1410ecddfe22f35048bb02866ad9e5bb90f62e9c
Trashah <l21530323@cancun.tecnm.mx>
2024-10-29 00:28:03 -0500

    Cambios a index.php, cambios en navbar de todas las páginas, traducción de login.php, creación de pedidos.php

---
+++ php/pedidos.php
@@ -0,0 +1,99 @@
+<?php
+
+include '../.gitignore/config.php';
+include '../php/functions.php';
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+	
+    <meta charset="utf-8">
+	<meta name = "viewport" content="width=device-width, initial-scale=1">
+	<title> HammaCraft </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <script src = "../javascript/script.js" defer> </script>
+    <style>
+        body {
+            background-color: #e5fefe; /* Morado claro */
+        }
+        .navbar {
+            background-color: #fbffff; /* Azul celeste */
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <header class = "header-hammacraft">
+
+        <nav class = "nav-hammacraft">
+                <a href = "https://www.hammacraft.lat/">
+                <img class = "logo-item" src="../images/logo.png" alt="Logo"> 
+                </a>
+            <div class = "search-bar-hammacraft">
+                <input type = "text" placeholder = "Buscar...">
+            </div>
+            <div class = "nav-hammacraft-links">
+                <a href="https://www.hammacraft.lat/">Pantalla principal</a>
+                <a href="https://www.hammacraft.lat/php/catalogo.php">Catálogo</a>
+                <a href="https://www.hammacraft.lat/php/login.php">Iniciar Sesión</a>
+                <a href="https://www.hammacraft.lat/php/pedidos.php">Pedidos</a>
+                <button class = "cart-btn-hammacraft">
+                    <i class="fas fa-shopping-cart"></i> Carrito
+                </button>
+                <a href="https://www.hammacraft.lat/php/login.html">
+                    <button class = "cart-btn-hammacraft">
+                    <i class="fa-solid fa-user"></i> Iniciar Sesión
+                    </button>
+                </a>
+            </div>
+        </nav>
+
+    </header>
+
+    <main>
+
+        <h1>EN MANTENIMIENTO HOLAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAGRIA</h1>
+
+    </main>
+
+    <footer>
+
+        <div class="footer-content">
+
+            <div class="footer-section">
+
+                <h3>MENU</h3>
+
+                <ul>
+
+                    <li><a href="https://www.hammacraft.lat">Pantalla principal</a></li>
+                    <li><a href="https://www.hammacraft.lat/php/catalogo.php">Tienda</a></li>
+
+            <div class="footer-section">
+
+                <h3>Siguenos</h3>
+
+                <ul>
+
+                    <li><a href="#facebook">Facebook</a></li>
+                    <li><a href="#twitter">Twitter</a></li>
+                    <li><a href="#instagram">Instagram</a></li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </footer>
+
+</body>
+</html>
