73a040375445dcc5b77d921e2a4fe7ece20b96e1
LinkDiego <diego.shulk@gmail.com>
2024-11-13 20:24:57 -0500

    Update catalogo.php

--- php/catalogo.php
+++ php/catalogo.php
@@ -19,6 +19,7 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../css/styles.css">
     <script src = "../javascript/script.js" defer> </script>
+    <script src="javascript\catalago_carrito.js" defer></script>
     <style>
         body {
             background-color: #e5fefe; /* Morado claro */
