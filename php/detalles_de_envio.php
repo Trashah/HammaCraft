31790fcb8467630968b8f4b247287a1a28f77c08
LinkDiego <diego.shulk@gmail.com>
2024-11-19 20:33:31 -0500

    Update detalles_de_envio.php

--- php/detalles_de_envio.php
+++ php/detalles_de_envio.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Detalles de Envío</title>
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/metodo_de_pago.css">
 </head>
 <body>
 
