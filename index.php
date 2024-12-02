93e7c24c9664b857ac800411336ac6ce3781c88e
HenrrydeJesusMaganaMelchi <henrrymelchi@gmail.com>
2024-12-02 10:28:24 -0500

    added seccion pedidos personalizados

--- index.php
+++ index.php
@@ -68,7 +68,17 @@ session_start();
             </div>
 
         </section>
-
+        <div class="custom-section">
+        <div class="image-container">
+            <img class="image-placeholder" src="atractive_hamabeads.png" alt="hamma_beads">
+        </div>
+        <div class="text-container">
+            <p>¡Haz que tus personajes favoritos cobren vida! 🎮🌟 Desde héroes de videojuegos hasta íconos de tus series animadas preferidas, personalizamos tus diseños en Hama Beads para que sean tan únicos como tú. ¡Haz tu pedido ahora y lleva tu pasión al siguiente nivel!</p>
+            <div class="button-container">
+                <a href="/php/pedidos.php" class="custom-button">Hacer pedido</a>
+            </div>
+        </div>
+    </div>
         <section>
 
         </section>
