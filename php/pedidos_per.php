4a9a38121eda1ed4f09cb5ee3f98475f9314b794
HenrrydeJesusMaganaMelchi <henrrymelchi@gmail.com>
2024-12-02 10:50:46 -0500

    testing section per

--- php/pedidos_per.php
+++ php/pedidos_per.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sección Personalizada</title>
-    <link rel="stylesheet" href="pedidos_per.css">
-</head>
-<body>
+
     <div class="custom-section">
         <div class="image-container">
             <img class="image-placeholder" src="atractive_hamabeads.png" alt="hamma_beads">
@@ -18,5 +10,4 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+
