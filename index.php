ea7233fa915c93491aee1e6998002767ae6cd319
HenrrydeJesusMaganaMelchi <henrrymelchi@gmail.com>
2024-12-02 11:25:01 -0500

    testing 2 section per

--- index.php
+++ index.php
@@ -69,17 +69,17 @@ session_start();
             </div>
         </section>
         <section class="pedidos_oc">
-            <div class="custom-section">
-                <div class="image-container">
-                    <img class="image-placeholder" src="/images/atractive_hamabeads.png" alt="hamma_beads">
-                </div>
-                <div class="text-container">
-                    <p>¡Haz que tus personajes favoritos cobren vida! 🎮🌟 Desde héroes de videojuegos hasta íconos de tus series animadas preferidas, personalizamos tus diseños en Hama Beads para que sean tan únicos como tú. ¡Haz tu pedido ahora y lleva tu pasión al siguiente nivel!</p>
-                    <div class="button-container">
-                        <a href="/php/pedidos.php" class="custom-button">Hacer pedido</a>
-                    </div>
-                </div>
+        <div class="custom-section">
+        <div class="image-container">
+            <img class="image-placeholder" src="/images/atractive_hamabeads.png" alt="hamma_beads">
+        </div>
+        <div class="text-container">
+            <p>¡Haz que tus personajes favoritos cobren vida! 🎮🌟 Desde héroes de videojuegos hasta íconos de tus series animadas preferidas, personalizamos tus diseños en Hama Beads para que sean tan únicos como tú. ¡Haz tu pedido ahora y lleva tu pasión al siguiente nivel!</p>
+            <div class="button-container">
+                <a href="/php/pedidos.php" class="custom-button">Hacer pedido</a>
             </div>
+        </div>
+    </div>
         </section>
 
         
