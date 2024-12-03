8a3cb4fc824cf4563b6d6c7f9a9993d96ff1b967
Trashah <l21530323@cancun.tecnm.mx>
2024-12-02 22:45:35 -0500

    Prepared admin.php and usuario.php

---
+++ php/admin.php
@@ -0,0 +1,47 @@
+<?php
+
+include 'functions.php';
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+
+<html lang = "es">
+
+<head>
+	
+    <meta charset="utf-8">
+	<meta name = "viewport" content = "width=device-width, initial-scale=1">
+
+	<title> HammaCraft - Página de admin </title>
+
+    <link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel = "stylesheet" href = "/css/styles.css">
+
+    <style>
+        body {
+            background-color: #e5fefe; /* Morado claro */
+        }
+        .navbar {
+            background-color: #fbffff; /* Azul celeste */
+        }
+    </style>
+
+</head>
+
+<body class = "body-hammacraft">
+
+    <?php include 'header.php'; ?>
+
+    <main>
+
+    </main>
+
+    <?php include "footer.php"; ?>
+
+</body>
+
+</html>
