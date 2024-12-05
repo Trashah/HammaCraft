d65681bca3df5b4de3b533e0e34acf56170e38e5
Andy-LH123 <116239005+Andy-LH123@users.noreply.github.com>
2024-12-05 00:43:54 -0500

    admin

--- php/functions.php
+++ php/functions.php
@@ -136,6 +136,9 @@ function tablasAdmin() {
         echo "<td>$row[1]</td>";
         echo "<td>$row[2]</td>";
         echo "<td>$row[3]</td>";
+        echo "<td>$row[4]</td>";
+        echo "<td>$row[5]</td>";
+        echo "<td>$row[6]</td>";
         echo "</tr>";
     }
 
