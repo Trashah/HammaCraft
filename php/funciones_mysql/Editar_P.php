2a49cb7e8d1b22f63dcfbdc32efa0af0409e9250
SmukkeFake <SmukkeFake@gmail.com>
2024-12-05 20:04:43 -0500

    admin

--- php/funciones_mysql/Editar_P.php
+++ php/funciones_mysql/Editar_P.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     mysqli_close($conexion);
-    header("Location: ../admin.php");
+    header("https://www.hammacraft.lat/php/admin.php");
     exit;
 }
 ?>
