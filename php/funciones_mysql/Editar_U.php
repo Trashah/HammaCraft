538ad3b09e5f03faecc7d459217f34b5d80da817
SmukkeFake <SmukkeFake@gmail.com>
2024-12-09 01:16:50 -0500

    sdasd

--- php/funciones_mysql/Editar_U.php
+++ php/funciones_mysql/Editar_U.php
@@ -30,9 +30,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql = "UPDATE usuarios SET NombreDeUsuario = '$nombredeusuario', Nombre = '$nombre', Apellido = '$apellido', 
     Email = '$email' WHERE ID = $id";
 
+    $sql2 = "INSERT INTO BITACORA(Usuario,Fecha,Hora,Operacion) VALUES('superadmin',CURDATE(),DATETIME(),'UPDATE Usuarios')";
+
     // Ejecutar la consulta
     if (mysqli_query($conexion, $sql)) {
         echo "Registro actualizado correctamente.";
+
         // Redirigir antes de cualquier salida
         header("Location: ../admin.php");
         exit; // Asegúrate de que el script se detenga después de la redirección
