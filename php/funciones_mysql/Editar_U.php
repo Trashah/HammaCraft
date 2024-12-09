6d0c137dde0719c7ca45c46eb8915b61b4b3fe73
SmukkeFake <SmukkeFake@gmail.com>
2024-12-09 01:06:19 -0500

    aaaaaaaa

--- php/funciones_mysql/Editar_U.php
+++ php/funciones_mysql/Editar_U.php
@@ -6,13 +6,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $apellido = $_POST['apellido'];
     $email = $_POST['email'];
 
-    // Sanitización de entradas para evitar SQL injection
-    $id = intval($id); // Para asegurarse de que el ID sea un número
-    $nombredeusuario = mysqli_real_escape_string($conexion, $nombredeusuario);
-    $nombre = mysqli_real_escape_string($conexion, $nombre);
-    $apellido = mysqli_real_escape_string($conexion, $apellido);
-    $email = mysqli_real_escape_string($conexion, $email);
-
     // Establecer conexión a la base de datos
     $servidor = "localhost";
     $usuario = "root";
@@ -21,21 +14,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $conexion = mysqli_connect($servidor, $usuario, $password, $bd);
 
-    if ($conexion) {
-        // Construir la consulta SQL
-        $sql = "UPDATE usuarios SET NombreDeUsuario = '$nombredeusuario', Nombre = '$nombre', Apellido = '$apellido', 
-        Email = '$email' WHERE ID = $id";
+    // Verificar si la conexión fue exitosa
+    if (!$conexion) {
+        die("Error de conexión a la base de datos: " . mysqli_connect_error());
+    }
 
-        // Ejecutar la consulta
-        if (mysqli_query($conexion, $sql)) {
-            // Redirigir antes de cualquier salida
-            header("Location: ../admin.php");
-            exit; // Asegúrate de que el script se detenga después de la redirección
-        } else {
-            echo "Error al actualizar el registro: " . mysqli_error($conexion);
-        }
+    // Sanitización de entradas para evitar SQL injection
+    $id = intval($id); // Para asegurarse de que el ID sea un número
+    $nombredeusuario = mysqli_real_escape_string($conexion, $nombredeusuario);
+    $nombre = mysqli_real_escape_string($conexion, $nombre);
+    $apellido = mysqli_real_escape_string($conexion, $apellido);
+    $email = mysqli_real_escape_string($conexion, $email);
+
+    // Construir la consulta SQL
+    $sql = "UPDATE usuarios SET NombreDeUsuario = '$nombredeusuario', Nombre = '$nombre', Apellido = '$apellido', 
+    Email = '$email' WHERE ID = $id";
+
+    // Ejecutar la consulta
+    if (mysqli_query($conexion, $sql)) {
+        // Redirigir antes de cualquier salida
+        header("Location: ../admin.php");
+        exit; // Asegúrate de que el script se detenga después de la redirección
     } else {
-        echo "Error de conexión a la base de datos.";
+        echo "Error al actualizar el registro: " . mysqli_error($conexion);
     }
 
     // Cerrar la conexión
